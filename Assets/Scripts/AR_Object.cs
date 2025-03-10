8e90494da377bd7825b58b110fb1ec6f969cff9f
Kraaven <dhruv.bhojaraj@gmail.com>
2025-03-10 15:00:22 +0530

    Started Adding Objects
    - Fixed UI Input
    - Added FLIR Camera
    - Added Movidius Chip

--- Assets/Scripts/AR_Object.cs
+++ Assets/Scripts/AR_Object.cs
@@ -9,10 +9,11 @@ using UnityEngine.UI;
 public class AR_Object : MonoBehaviour
 {
     [SerializeField] private Canvas MainUI;
-    [SerializeField] private Button InfoButton;
+    //[SerializeField] private Button InfoButton;
     [SerializeField] private TMP_Text TitleName;
     [SerializeField] private GameObject[] informationObjects;
     private bool DataVisibilityToggle;
+    
 
     public void Start()
     {
@@ -21,17 +22,23 @@ public class AR_Object : MonoBehaviour
             informationObject.SetActive(false);
         }
         
-        InfoButton.onClick.AddListener(() =>
-        {
-            DataVisibilityToggle = !DataVisibilityToggle;
-
-            foreach (var informationObject in informationObjects)
-            {
-                informationObject.SetActive(DataVisibilityToggle);
-            }
-        });
-        
+        //MainUI.rootCanvas.worldCamera = Camera.main;
+        MainUI.worldCamera = Camera.main;
     }
-    
-    
+
+    public void ToggleButtons()
+    {
+        DataVisibilityToggle = !DataVisibilityToggle;
+
+        foreach (var informationObject in informationObjects)
+        {
+            informationObject.SetActive(DataVisibilityToggle);
+        }
+    }
+
+    public void Update()
+    {
+        MainUI.transform.LookAt(Camera.main.transform.position, Vector3.up);
+        MainUI.transform.rotation = Quaternion.Euler(0,MainUI.transform.rotation.eulerAngles.y+180,0);
+    }
 }
